46f57de8031a396085b7cf3836214525623c4d61
Daniel Lobato <daniel.lobato2001@gmail.com>
2022-10-28 21:35:11 -0300

    feat: Creates react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="crf_star.png" className="App-logo" alt="logo" />
+        <p className="club-name">Flamengo</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
